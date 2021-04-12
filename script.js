8b938e88bebab2e21b6c592d525014afb5fc9e84
WombleWoo7547 <73031179+WombleWoo7547@users.noreply.github.com>
2021-04-12 12:06:39 +0100

    finishing touches

--- script.js
+++ script.js
@@ -24,46 +24,48 @@ $(() => {
   button.click(() => {
     alert("WOUW");
   });
-});
-function showUsers(users) {
-  try {
-    document.querySelector("div.users").innerHTML = "";
-    users.forEach((user) => {
-      const wrapperElement = document.createElement("div");
-
-      const name = document.createElement("h2");
-      name.textContent = `${user.name}`;
-      console.log(name);
-
-      const score = document.createElement("h1");
-      score.textContent = `${user.ss}`;
-      console.log(score);
-
-      const button1 = document.createElement("button");
-      button1.textContent = `+`;
-      button1.classList.add("btn");
-      button1.addEventListener("click", () => {
-        user.ss++;
-        showUsers(users);
+  function showUsers(users) {
+    try {
+      document.querySelector("div.users").innerHTML = "";
+      users.forEach((user) => {
+        const wrapperElement = document.createElement("div");
+  
+        const name = document.createElement("h2");
+        name.textContent = `${user.name}`;
+        console.log(name);
+  
+        const score = document.createElement("h1");
+        score.textContent = `${user.ss}`;
+        console.log(score);
+  
+        const button1 = document.createElement("button");
+        button1.textContent = `+`;
+        button1.classList.add("btn");
+        button1.addEventListener("click", () => {
+          user.ss++;
+          showUsers(users);
+        });
+        console.log(button1);
+  
+        const button2 = document.createElement("button");
+        button2.textContent = `-`;
+        button2.classList.add("btn");
+        button2.addEventListener("click", () => {
+          user.ss--;
+          showUsers(users);
+        });
+        console.log(button2);
+  
+        wrapperElement.append(name);
+        wrapperElement.append(score);
+        wrapperElement.append(button1);
+        wrapperElement.append(button2);
+        document.querySelector("div.users").append(wrapperElement);
+        user.ss = '';
+        user.name = ''
       });
-      console.log(button1);
-
-      const button2 = document.createElement("button");
-      button2.textContent = `-`;
-      button2.classList.add("btn");
-      button2.addEventListener("click", () => {
-        user.ss--;
-        showUsers(users);
-      });
-      console.log(button2);
-
-      wrapperElement.append(name);
-      wrapperElement.append(score);
-      wrapperElement.append(button1);
-      wrapperElement.append(button2);
-      document.querySelector("div.users").append(wrapperElement);
-    });
-  } catch (err) {
-    console.error(err);
+    } catch (err) {
+      console.error(err);
+    }
   }
-}
+});
